71c23d5405fdc3ccfd030c6f789017b200a1b7a6
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2022-07-19 20:03:27 +0200

    Hm, dont think i did much here either

--- client/src/components/MainMenu.jsx
+++ client/src/components/MainMenu.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const MainMenu = () => {
+
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
     return (
         <nav>
             <ul>
@@ -11,9 +15,9 @@ const MainMenu = () => {
                 <li>
                     <Link to="/create">Create Sneaker</Link>
                 </li>
-                <li>
+                {isAuthenticated && <li>
                     <Link to="/profile">Profile</Link>
-                </li>
+                </li>}
                 <li>
                     <Link to="/login">Login</Link>
                 </li>
