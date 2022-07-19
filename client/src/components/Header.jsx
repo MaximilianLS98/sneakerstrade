93bdfe0d75294ae11ee6be817fe3bf3df9892fc0
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2022-07-19 22:50:49 +0200

    did header styling as well

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import MainMenu from "./MainMenu";
+import '../header.css';
 
 const Header = () => {
     return (
         <header>
-            <h1>Sneaker Store</h1>
+            <h1 className="header-title">SneakerSpot</h1>
             <MainMenu />
         </header>
     );
